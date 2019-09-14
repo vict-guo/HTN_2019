2a86af98f8e93335e3a8fb327c7cf311750d108c
vict-guo <victor.guo02@gmail.com>
2019-09-14 18:53:44 -0400

    setup

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
+public class Main{
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/SharpImage.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+
+public class SharpImage{
+    private String path;
+    public SharpImage(String s){
+        path = s;
+    }
+}
